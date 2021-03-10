f35b1c988e255062dca8d4d3466cde1a1d42aeec
kyungryun <kyung_ryun@naver.com>
2021-03-10 18:12:29 +0900

    Initial commit

---
+++ src/main/java/com/ryun/hellospring/SpringConfig.java
@@ -0,0 +1,35 @@
+package com.ryun.hellospring;
+
+import com.ryun.hellospring.aop.TimeTraceAop;
+import com.ryun.hellospring.repository.*;
+import com.ryun.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+////        return new JpaMemberRepository(em);
+//
+//    }
+}
---
+++ src/main/java/com/ryun/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package com.ryun.hellospring.repository;
+
+import com.ryun.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository  extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
